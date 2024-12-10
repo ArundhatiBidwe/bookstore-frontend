902b8290721d174f7197fe04c9b178954b4091bc
Arundhati Bidwe <>
2024-12-10 01:49:11 -0500

    changed css files and deleted codes

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,6 @@ import Cart from "./components/Cart";
 import Checkout from "./components/Checkout";
 import Navbar from "./components/Navbar";
 import "./styles.css";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import BookDetails from "./components/BookDetails";
 import Notes from "./pages/Notes";
 
@@ -25,8 +21,6 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/book/:id" element={<BookDetails />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/notes" element={<Notes />} />
       </Routes>
     </Router>
